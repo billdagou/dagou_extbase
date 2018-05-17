cd7cad341e5e4f1342e2073b9355bfeab106d11f
Bill.Dagou <billdagou@gmail.com>
2018-05-17 21:21:20 +0800

    EidRequestHandler

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-
+/*
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Frontend\Http\EidRequestHandler::class] = [
     'className' => \Dagou\DagouExtbase\Http\EidRequestHandler::class,
-];
+];*/
