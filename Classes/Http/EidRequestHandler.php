5c9740201ad6d09368fbb47af2be7cab0d596cb8
Bill.Dagou <billdagou@gmail.com>
2018-05-17 22:15:30 +0800

    EidRequestHandler

--- Classes/Http/EidRequestHandler.php
+++ Classes/Http/EidRequestHandler.php
@@ -262,7 +262,6 @@ class EidRequestHandler extends RequestHandler {
                 ],
             ]
         );
-        print_r($configuration);
 
         $this->controller->content = $bootstrap->run('', $configuration);
     }
