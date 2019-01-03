a56d82299d016849d2a3012895f21dc036b5d836
Bill.Dagou <billdagou@gmail.com>
2019-01-03 15:06:09 +0800

    Refactor and upate

---
+++ Classes/Traits/Condition.php
@@ -0,0 +1,71 @@
+<?php
+namespace Dagou\DagouExtbase\Traits;
+
+trait Condition {
+    /**
+     * @var int
+     */
+    protected $limit;
+    /**
+     * @var array
+     */
+    protected $orderings;
+    /**
+     * @var int
+     */
+    protected $page;
+
+    /**
+     * @return int
+     */
+    public function getLimit() {
+        return $this->limit;
+    }
+
+    /**
+     * @param int $limit
+     *
+     * @return \Dagou\DagouExtbase\DomainObject\ConditionInterface
+     */
+    public function setLimit(int $limit = 0) {
+        $this->limit = $limit > 0 ? $limit : 0;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOrderings() {
+        return $this->orderings;
+    }
+
+    /**
+     * @param array $orderings
+     *
+     * @return \Dagou\DagouExtbase\DomainObject\ConditionInterface
+     */
+    public function setOrderings(array $orderings = []) {
+        $this->orderings = $orderings;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPage() {
+        return $this->page;
+    }
+
+    /**
+     * @param int $page
+     *
+     * @return \Dagou\DagouExtbase\DomainObject\ConditionInterface
+     */
+    public function setPage(int $page = 1) {
+        $this->page = $page > 1 ? $page : 1;
+
+        return $this;
+    }
+}
