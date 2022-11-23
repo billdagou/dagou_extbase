58b674d8cf2d9cdcb847b3b6b9b7d06eba2bd8a8
Bill.Dagou <billdagou@gmail.com>
2022-11-23 13:20:50 +0800

    Update RteService

--- Classes/Service/RteService.php
+++ Classes/Service/RteService.php
@@ -5,9 +5,11 @@ use TYPO3\CMS\Core\Configuration\Richtext;
 use TYPO3\CMS\Core\Html\RteHtmlParser;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 
 class RteService implements SingletonInterface {
     protected RteHtmlParser $rteHtmlParser;
+    protected ConfigurationManagerInterface $configurationManager;
 
     /**
      * @param \TYPO3\CMS\Core\Html\RteHtmlParser $rteHtmlParser
@@ -17,17 +19,24 @@ class RteService implements SingletonInterface {
     }
 
     /**
+     * @param \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager
+     */
+    public function injectConfigurationManager(ConfigurationManagerInterface $configurationManager) {
+        $this->configurationManager = $configurationManager;
+    }
+
+    /**
      * @param string $value
      * @param string $table
      * @param string $field
-     * @param string $recordType
+     * @param string|null $recordType
      *
      * @return string
      */
-    public function transformDbToRte(string $value, string $table, string $field, string $recordType = '0'): string {
-        return $this->rteHtmlParser->transformTextForPersistence(
+    public function transformDbToRte(string $value, string $table, string $field, ?string $recordType = NULL): string {
+        return $this->rteHtmlParser->transformTextForRichTextEditor(
             $value,
-            $this->getRteConfiguration($table, $field, $recordType)
+            $this->getRteConfiguration($table, $field, $recordType)['proc.'] ?? []
         );
     }
 
@@ -35,35 +44,47 @@ class RteService implements SingletonInterface {
      * @param string $value
      * @param string $table
      * @param string $field
-     * @param string $recordType
+     * @param string|null $recordType
      *
      * @return string
      */
-    public function transformRteToDb(string $value, string $table, string $field, string $recordType = '0'): string {
+    public function transformRteToDb(string $value, string $table, string $field, ?string $recordType = NULL): string {
         return $this->rteHtmlParser->transformTextForPersistence(
             $value,
-            $this->getRTEConfiguration($table, $field, $recordType)
+            $this->getRTEConfiguration($table, $field, $recordType)['proc.'] ?? []
         );
     }
 
     /**
      * @param string $table
      * @param string $field
-     * @param string $recordType
+     * @param string|null $recordType
      *
      * @return array
      */
-    protected function getRteConfiguration(string $table, string $field, string $recordType): array {
+    protected function getRteConfiguration(string $table, string $field, ?string $recordType = NULL): array {
         return GeneralUtility::makeInstance(Richtext::class)
             ->getConfiguration(
                 $table,
                 $field,
-                $GLOBALS['TSFE']->id ?? 0,
-                $recordType,
-                [
-                    'type' => 'text',
-                    'enableRichtext' => TRUE,
-                ]
-            )['proc.'] ?? [];
+                $this->configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK)['persistence']['storagePid'],
+                $recordType ?? $this->getTcaTypeValue($table),
+                $GLOBALS['TCA'][$table]['columns'][$field]['config']
+            );
+    }
+
+    /**
+     * @param string $table
+     *
+     * @return string
+     */
+    protected function getTcaTypeValue(string $table): string {
+        $typeNum = 0;
+
+        if (!isset($GLOBALS['TCA'][$table]['types'][$typeNum]) || !$GLOBALS['TCA'][$table]['types'][$typeNum]) {
+            $typeNum = isset($GLOBALS['TCA'][$table]['types']['0']) ? 0 : 1;
+        }
+
+        return $typeNum;
     }
 }
